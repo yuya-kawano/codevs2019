8130d818b20a927edcc9196dd46fb45bc7dd12df
y_kawano <kawano905@yahoo.co.jp>
2019-04-18 09:18:59 +0900

    iroiro

--- codevs/main.cpp
+++ codevs/main.cpp
@@ -74,10 +74,26 @@ int _dy[] = { 0, 1, 0, -1, 1, -1, 1, -1 };
 
 const int WIDTH = 10;
 const int HEIGHT = 16;
+const int BOTTOM = HEIGHT - 1;
 const int BLOCK_NUM = 500;
+const u64 mask4 = 0b1111;
 
 
-const int mask4 = 0b1111;
+bool IsIn(int x, int y)
+{
+	return 0 <= x && x < WIDTH && 0 <= y && y < HEIGHT;
+}
+
+
+class Point
+{
+public:
+	int x;
+	int y;
+
+	Point() { this->x = 0; this->y = 0; }
+	Point(int x, int y) { this->x = x; this->y = y; }
+};
 
 //1 2
 //3 4
@@ -90,8 +106,16 @@ public:
 
 	int Get(int x, int y)
 	{
-		return (map[x] >> ((HEIGHT - y - 1) << 2) & mask4);
+		return ((ull)map[x] >> ((BOTTOM - y) * 4) & mask4);
 	}
+
+	void SetBit(int x, int y, int n)
+	{
+		int shift = ((BOTTOM - y) * 4);
+		map[x] &= (~(mask4 << shift));
+		map[x] |= (n << shift);
+	}
+
 
 	void Put(int block, int pos, int rot)
 	{
@@ -132,23 +156,95 @@ public:
 		if ((l & mask4) == 0) l >>= 4;
 		if ((r & mask4) == 0) r >>= 4;
 
-		Set(pos, l);
-		Set(pos + 1, r);
+		int check = 0;
+		DropLine(pos, l, &check);
+		DropLine(pos + 1, r, &check);
 	}
 
-	void Set(int pos, int line)
+	int DropLine(int x, int line, int *check)
 	{
-		if (map[pos] == 0)
+		if (line == 0) return 0;
+
+		int ret = 0;
+
+		if (map[x] == 0)
 		{
-			map[pos] |= line;
+			map[x] |= line;
+
+			*check |= (BOTTOM << (x * 4));
 		}
 		else
 		{
-			int p = bsr((ull)map[pos]);
+			int p = bsr((ull)map[x]);
 			p /= 4;
 			p += 1;
+
+			*check |= ((BOTTOM - p) << 4);
+
 			p *= 4;
-			map[pos] |= (line << p);
+			map[x] |= (line << p);
+		}
+	}
+
+	void Submit(int first_check)
+	{
+		bool is_update = false;
+
+		int check = first_check;
+		int next = first_check;
+
+		static Point q[WIDTH * HEIGHT] = {};
+		int q_cnt = 0;
+
+		for (int x = 0; x < WIDTH; x++)
+		{
+			int bottom = ((check >> (x * 4)) & mask4);
+			if (bottom > 0)
+			{
+				for (int y = bottom; y >= 0; y--)
+				{
+					for (int d = 0; d < 8; d++)
+					{
+						int dx = x;
+						int dy = y;
+						int sum = Get(x, y);
+
+						while (true)
+						{
+							dx += _dx[d];
+							dy += _dy[d];
+
+							if (!IsIn(dx, dy)) break;
+
+							int n = Get(dx, dy);
+
+							if (n == 0) break;
+
+							sum += n;
+
+							if (sum > 10) break;
+
+							if (sum == 10)
+							{
+								int qx = dx;
+								int qy = dy;
+								while (true)
+								{
+									q[q_cnt].x = qx;
+									q[q_cnt].y = qy;
+									q_cnt++;
+
+									if (qx == x && qy == y) break;
+
+									qx -= _dx[d];
+									qy -= _dy[d];
+								}
+								break;
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 };
@@ -220,19 +316,55 @@ int main()
 	State state;
 	memset(state.map, 0, sizeof(state.map));
 
-	int block1 = (1 | (2 << 4) | (3 << 8) | (4 << 12));
-	int block2 = (1 | (2 << 4) | (0 << 8) | (4 << 12));
+	//int block1 = (1 | (2 << 4) | (3 << 8) | (4 << 12));
+	//int block2 = (1 | (2 << 4) | (0 << 8) | (4 << 12));
 
-	state.Put(block1, 0, 0);
-	state.Put(block2, 0, 0);
-	int n1 = state.Get(0, HEIGHT - 1);
-	int n2 = state.Get(0, HEIGHT - 1 - 1);
-	int n3 = state.Get(0, HEIGHT - 1 - 2);
-	int n4 = state.Get(0, HEIGHT - 1 - 3);
+	//1 2
+	//3 4
 
-	while (true)
+	//1 2
+	//  4
+
+	//state.Put(block1, 0, 0);
+	//state.Put(block2, 0, 0);
+
+	//state.Put(block1, 2, 1);
+	//state.Put(block2, 2, 1);
+
+	//state.Put(block1, 4, 2);
+	//state.Put(block2, 4, 2);
+
+	//state.Put(block1, 6, 3);
+	//state.Put(block2, 6, 3);
+
+	state.SetBit(0, 15, 1);
+
+	state.SetBit(1, 15, 3);
+	state.SetBit(1, 14, 5);
+	state.SetBit(1, 13, 7);
+	state.SetBit(1, 12, 8);
+
+	state.SetBit(2, 15, 3);
+	state.SetBit(2, 14, 1);
+
+	state.SetBit(3, 15, 1);
+
+	int block = (5 | (5 << 4) | (0 << 8) | (9 << 12));
+	state.Put(block, 0, 0);
+
+	for (int y = 0; y < HEIGHT; y++)
+	{
+		for (int x = 0; x < WIDTH; x++)
+		{
+			cout << state.Get(x, y);
+			cout << " ";
+		}
+		cout << endl;
+	}
+
+	/*while (true)
 	{
 		Input();
 		cout << "2 0" << endl;
-	}
+	}*/
 }
